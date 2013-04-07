dda47466566bf6eb90672d7defb5746fd1da2bc0
Terkel Brix <terkel.brix@gmail.com>
2013-04-08 01:13:40 +0200

    Enums for Brands added

---
+++ ProjectCrankUpTheHeat/src/org/northennerds/enums/Brands.java
@@ -0,0 +1,17 @@
+package org.northennerds.enums;
+
+public enum Brands {
+	Panasonic("Panasonic"),Haier("Haier"), Daikin("Daikin"), LG("LG"), Bosch("Bosch"), IVT("IVT"),Electrolux_old("Elux1"), Mitsubishi("Mitsubishi"),
+	Toshiba("Toshiba"), Electrolux_new("Elux2"), Fujitsu("Fujitsu"), Zibro("");
+
+	private String name;
+
+	Brands(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+}
